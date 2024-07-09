15c9209aeddf4e7f11c54a104f6032934067dac1
XsJota <jhonnatan.jncp@gmail.com>
2024-07-09 15:08:51 -0300

    feat: function that handles the submit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,17 +4,31 @@ import "./Navbar.css";
 // import icons
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
-// import LInk
-import { Link } from "react-router-dom";
+// import Link
+import { Link, useNavigate } from "react-router-dom";
+
+// import hook
+import { useState } from "react";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch('');
+  }
   return (
     <nav id="nav">
         <h2>
             <Link to='/'><BiCameraMovie /> Film</Link>
         </h2>
-        <form>
-            <input type="text" placeholder="Busque um filme" />
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)} />
             <button>
                 <BiSearchAlt2 />
             </button>
